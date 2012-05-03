e9155e97400767872c39e7c669353f70584130f6
Marcin Chyłek <marcin@chylek.pl>
2012-05-03 18:27:37 +0200

    Add EelMonitor.php, version

---
+++ src/EelMonitor/EelMonitor.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * This file is part of Eel-Monitor.
+ *
+ * (c) Marcin Chyłek <marcin@chylek.pl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace EelMonitor;
+
+class EelMonitor
+{
+
+    const VERSION = '1.0.0-dev';
+
+}
